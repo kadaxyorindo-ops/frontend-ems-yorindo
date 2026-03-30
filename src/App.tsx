eef65b7fc9e853f55de894fe9a648455118caf4d
daff00 <daffakpc21@gmail.com>
2026-03-30 11:52:19 +0700

    feat(layout): Remove unused libraries import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "@/pages/auth/Login";
 import { Events } from "@/pages/events/Events";
 import { Communication } from "@/pages/communication/Communication";
